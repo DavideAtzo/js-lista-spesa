de0876f1fdb74ef220cbe7924a30b07e98c63b50
DavideAtzo <atzo9477@gmail.com>
2023-02-28 15:36:34 +0100

    final commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const shoppingList = ['Pane', 'Mortadella', 'Formaggio', 'Vino', 'Mirto', 'Olio'];
+const ul = document.querySelector('.shopping-list');
+console.log(shoppingList);
+
+let i = 0;
+while(i < shoppingList.length){
+    const li = document.createElement('li');
+    li.innerText = (shoppingList[i]);
+    ul.append(li);
+    i++;
+}
